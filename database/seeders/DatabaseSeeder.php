e6f26f33405d0fbb6d75e2b8fae2ffa29561b682
Andrei Golubev <andrey.b.golubev@proton.me>
2024-08-06 01:43:42 +0545

    Add passport client creation to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,12 @@ namespace Database\Seeders;
 
 use App\Models\Order;
 use App\Models\User;
+use Laravel\Passport\Client;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Worker;
 use Illuminate\Database\Seeder;
+use Laravel\Passport\Passport;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +18,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $client = Client::create([
+            'name' => 'Password API client',
+            'secret' => 'D1rIyRHavAc30l0Jb8C1aMeOReK280ScS2hquO4I',
+            'password_client' => true,
+            'personal_access_client' => false,
+            'revoked' => false,
+            'redirect' => 'http://localhost',
+        ]);
+
+        $this->command->info('Passport client created successfully!');
+        $this->command->info('Client ID: ' . $client->id);
+        $this->command->info('Client Secret: ' . $client->secret);
+
         $this->call([OrderTypeSeeder::class]);
 
         $user = User::factory()->create([
