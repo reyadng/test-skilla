08ad7a38dfb8979a49cb749f208ffedd3ffde62a
Andrei Golubev <andrey.b.golubev@proton.me>
2024-08-06 01:43:43 +0545

    Remove routes for test controller

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\OrderController;
-use App\Http\Controllers\Api\TestController;
 use App\Http\Controllers\Api\WorkerController;
 use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth:api')
-    ->get('/test', [TestController::class, 'index']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/orders', [OrderController::class, 'store']);
